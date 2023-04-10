1d2aa869cff9c214a40f753f5ddb665e898a4b1e
Dave Hill <dhill@ingramhill.com>
2023-04-10 12:30:08 -0400

    Remove stray byebug.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -9,7 +9,6 @@ class PatientsController < ApplicationController
   # GET /patients?name=   (preform FHIR search with name param)
   # GET /patients?page=   (directly fetch a pagination url from Bundle.link[i].url)
   def index
-    byebug
     if params[:name]
       fhir_response = @fhir_client.search(FHIR::Patient, search: {parameters: {name: params[:name]}})
       fhir_bundle = fhir_response.resource
